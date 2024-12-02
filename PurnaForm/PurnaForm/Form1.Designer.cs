33c84c321a273dafe6728ffc848b61bceed2607e
Purna Shrestha <crush9918@gmail.com>
2024-12-02 22:27:31 +0545

    feat: Create a first desktop form with username password and MessageBox

---
+++ PurnaForm/PurnaForm/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace PurnaForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(336, 169);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 20);
+            label1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(419, 23);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(125, 111);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 10F);
+            label2.Location = new Point(336, 189);
+            label2.Name = "label2";
+            label2.Size = new Size(87, 23);
+            label2.TabIndex = 2;
+            label2.Text = "Username";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 10F);
+            textBox1.Location = new Point(336, 218);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(295, 30);
+            textBox1.TabIndex = 3;
+            //textBox1.TextChanged += textBox1_TextChanged;
+            //this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 10F);
+            label3.Location = new Point(336, 280);
+            label3.Name = "label3";
+            label3.Size = new Size(80, 23);
+            label3.TabIndex = 4;
+            label3.Text = "Password";
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 10F);
+            textBox2.Location = new Point(336, 315);
+            textBox2.Name = "textBox2";
+            textBox2.PasswordChar = '*';
+            textBox2.Size = new Size(295, 30);
+            textBox2.TabIndex = 5;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.Highlight;
+            button1.Font = new Font("Segoe UI", 12F);
+            button1.ForeColor = SystemColors.ButtonHighlight;
+            button1.Location = new Point(336, 382);
+            button1.Name = "button1";
+            button1.Size = new Size(295, 50);
+            button1.TabIndex = 6;
+            button1.Text = "Log In";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(936, 553);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(label3);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(pictureBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Purna Form";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private PictureBox pictureBox1;
+        private Label label2;
+        private TextBox textBox1;
+        private Label label3;
+        private TextBox textBox2;
+        private Button button1;
+    }
+}
